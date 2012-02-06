e8f428d0e140e025907ad4d7a0d9ad1c05a0c0c7
Daniel Kang <me@daniel.gs>
2012-02-06 15:08:39 +0900

    small change in sample code

--- sample.cpp
+++ sample.cpp
@@ -7,7 +7,7 @@ int main()
     http server([](request& req, response& res){
 		res.set_status(200);
 		res.set_header("Content-Type", "text/plain");
-		res.end("C++ FTW!\n", [=](int status) {});
+		res.end("C++ FTW!\n", [](int status) {});
 	});
     std::cout << "Server running at http://0.0.0.0:8080/" << std::endl;
     server.listen("0.0.0.0", 8080);
