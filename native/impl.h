2f59fb98a5445ee17cb560dbd6b70cc689ef5f98
Daniel Kang <me@daniel.gs>
2012-02-16 17:57:53 +0900

    adding more fs functions to native::impl

--- native/impl.h
+++ native/impl.h
@@ -107,6 +107,7 @@ namespace native
             template<typename ...A>
             typename std::result_of<callback_t(A...)>::type invoke(A&& ... args)
             {
+                // TODO: isolate an exception caught in the callback.
                 return callback_(std::forward<A>(args)...);
             }
 
@@ -624,6 +625,191 @@ namespace native
         private:
             uv_tcp_t tcp_;
         };
+
+        static void on_fs_after(uv_fs_t* req)
+        {
+            switch(req->fs_type)
+            {
+            case UV_FS_CLOSE:
+            case UV_FS_RENAME:
+            case UV_FS_UNLINK:
+            case UV_FS_RMDIR:
+            case UV_FS_MKDIR:
+            case UV_FS_FTRUNCATE:
+            case UV_FS_FSYNC:
+            case UV_FS_FDATASYNC:
+            case UV_FS_LINK:
+            case UV_FS_SYMLINK:
+            case UV_FS_CHMOD:
+            case UV_FS_FCHMOD:
+            case UV_FS_CHOWN:
+            case UV_FS_FCHOWN:
+                if(req->result == -1)
+                {
+                    callbacks::invoke<std::function<void(error)>>(req->data, 0, error(static_cast<uv_err_code>(req->errorno)));
+                }
+                else
+                {
+                    callbacks::invoke<std::function<void(error)>>(req->data, 0, error());
+                }
+                break;
+
+            case UV_FS_UTIME:
+            case UV_FS_FUTIME:
+                callbacks::invoke<std::function<void()>>(req->data, 0);
+                break;
+
+            case UV_FS_OPEN:
+            case UV_FS_SENDFILE:
+            case UV_FS_WRITE:
+            case UV_FS_READ:
+                if(req->result == -1)
+                {
+                    callbacks::invoke<std::function<void(error, int)>>(req->data, 0, error(static_cast<uv_err_code>(req->errorno)), -1);
+                }
+                else
+                {
+                    callbacks::invoke<std::function<void(error, int)>>(req->data, 0, error(), req->result);
+                }
+                break;
+
+            case UV_FS_STAT:
+            case UV_FS_LSTAT:
+            case UV_FS_FSTAT:
+                // TODO: implement UV_FS_*STAT types
+                assert(false);
+                break;
+
+            case UV_FS_READLINK:
+                if(req->result == -1)
+                {
+                    callbacks::invoke<std::function<void(error, const std::string&)>>(req->data, 0, error(static_cast<uv_err_code>(req->errorno)), std::string());
+                }
+                else
+                {
+                    callbacks::invoke<std::function<void(error, const std::string&)>>(req->data, 0, error(), std::string(static_cast<char*>(req->ptr)));
+                }
+                break;
+
+            case UV_FS_READDIR:
+                if(req->result == -1)
+                {
+                    callbacks::invoke<std::function<void(error, const std::vector<std::string>&)>>(req->data, 0, error(static_cast<uv_err_code>(req->errorno)), std::vector<std::string>());
+                }
+                else
+                {
+                    char *namebuf = static_cast<char*>(req->ptr);
+                    int nnames = req->result;
+
+                    std::vector<std::string> names;
+
+                    for(int i = 0; i < nnames; i++)
+                    {
+                        std::string name(namebuf);
+                        names.push_back(name);
+
+#ifndef NDEBUG
+                        namebuf += name.length();
+                        assert(*namebuf == '\0');
+                        namebuf += 1;
+#else
+                        namebuf += name.length() + 1;
+#endif
+                    }
+
+                    callbacks::invoke<std::function<void(error, const std::vector<std::string>&)>>(req->data, 0, error(), names);
+                }
+                break;
+
+            default:
+                assert(false);
+                break;
+            }
+
+            // cleanup
+            delete reinterpret_cast<callbacks*>(req->data);
+            uv_fs_req_cleanup(req);
+            delete req;
+        }
+
+        // NOTE: Callback function is always invoked even when failed to initiat uv_functions.
+        //       However, when failed, callback function is invoked synchronously.
+        // F: function name
+        // P: parameters
+        // C: callback signature
+        template<typename F, typename C, typename ...P>
+        error fs_exec_async(F uv_function, C callback, P&&... params)
+        {
+            auto req = new uv_fs_t;
+            assert(req);
+
+            req->data = new callbacks(1);
+            assert(req->data);
+            callbacks::store(req->data, 0, callback);
+
+            int res = uv_function(uv_default_loop(), req, std::forward<P>(params)..., on_fs_after);
+            if(res < 0)
+            {
+                // failed: invoke callback function synchronously.
+                req->result = res;
+                req->path = nullptr;
+                req->errorno = uv_last_error(uv_default_loop()).code;
+                on_fs_after(req);
+            }
+
+            return error();
+        }
+
+        // TODO: Node.js implementation takes 'path' argument and pass it to exception if there's any.
+        // F: function name
+        // P: parameters
+        template<typename F, typename ...P>
+        error fs_exec_sync(F uv_function, P&&... params)
+        {
+            uv_fs_t req;
+
+            int res = uv_function(uv_default_loop(), &req, std::forward<P>(params)..., nullptr);
+            if(res < 0)
+            {
+                uv_fs_req_cleanup(&req);
+                return error(uv_last_error(uv_default_loop()));
+            }
+
+            uv_fs_req_cleanup(&req);
+            return error();
+        }
+
+        template<typename F, typename C, typename ...P>
+        error fs_exec(F uv_function, C callback, P&&... params)
+        {
+            if(callback) return fs_exec_async(uv_function, callback, std::forward<P>(params)...);
+            else return fs_exec_sync(uv_function, std::forward<P>(params)...);
+        }
+
+        error fs_close(int fd, std::function<void(error)> callback=nullptr)
+        {
+            return fs_exec(uv_fs_close, callback, fd);
+        }
+
+        error fs_sym_link(const std::string& dest, const std::string& src, bool dir=false, std::function<void(error)> callback=nullptr)
+        {
+            return fs_exec(uv_fs_symlink, callback, dest.c_str(), src.c_str(), dir?UV_FS_SYMLINK_DIR:0);
+        }
+
+        error fs_link(const std::string& orig_path, const std::string& new_path, std::function<void(error)> callback=nullptr)
+        {
+            return fs_exec(uv_fs_link, callback, orig_path.c_str(), new_path.c_str());
+        }
+
+        error fs_read_link(const std::string& path, std::function<void(error, const std::string&)> callback=nullptr)
+        {
+            return fs_exec(uv_fs_readlink, callback, path.c_str());
+        }
+
+        // TODO: implement fs_*stat() functions.
+        error fs_stat() { return error(); }
+        error fs_lstat() { return error(); }
+        error fs_fstat() { return error(); }
     }
 }
 
