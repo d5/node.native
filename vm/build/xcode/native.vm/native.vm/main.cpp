a9752aa31ed2628e36b712f048666b64b65e103d
Denny C. Dai <dennycd@me.com>
2013-08-27 16:22:16 -0700

    adding test main source code for parsing C++ file and output its content

---
+++ vm/build/xcode/native.vm/native.vm/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  native.vm
+//
+//  Created by Denny C. Dai on 2013-08-27.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#include <iostream>
+#include <clang-c/Index.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    CXIndex Index = clang_createIndex(0, 0);
+    CXTranslationUnit TU = clang_parseTranslationUnit(Index, 0, argv, argc, 0, 0, CXTranslationUnit_None);
+    
+    for(unsigned I = 0, N = clang_getNumDiagnostics(TU); I!=N; ++I){
+        CXDiagnostic diag = clang_getDiagnostic(TU, I);
+        CXString str = clang_format
+    }
+    
+    clang_disposeTranslationUnit(TU);
+    clang_disposeIndex(Index);
+    
+    return 0;
+}
